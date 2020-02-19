a6963d1a34b54609c71c1b2d1bd433ccb477c31c
R35 <R35@DESKTOP-IL5J0O6>
2020-02-19 14:48:21 +0900

    update

--- 1mysite02/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ 1mysite02/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -147,9 +147,11 @@ public class GuestbookRepository {
 		Connection conn = null;
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			//1. 드라이버 로딩
+			Class.forName( "org.mariadb.jdbc.Driver" );
 
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb";
+			//2. 연결하기
+			String url="jdbc:mysql://192.168.1.109:3307/webdb";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
