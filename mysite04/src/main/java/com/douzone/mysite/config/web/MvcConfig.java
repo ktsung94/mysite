fdbe1e7d119a67ea519bf59d67de894acf07ad4e
taesung <ktsung94@naver.com>
2020-04-14 14:59:00 +0900

    mysite04 - message Converter

--- mysite04/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/web/MvcConfig.java
@@ -1,6 +1,16 @@
 package com.douzone.mysite.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,10 +26,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setExposeContextBeansAsAttributes(true);
-		
+
 		return viewResolver;
 	}
-	
+
 	// Default Servlet Handler
 	// <!-- 서블릿 컨테이너의 디폴트 서블릿 위임 핸들러 -->
 	// <mvc:default-servlet-handler />
@@ -35,7 +45,37 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("file:D:\\mysite-upload/");
 	}
-	
-	
-	
+
+	// Message Converter
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		// asList는 argument를 배열로 만든다.
+		messageConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "html", Charset.forName("utf-8"))));
+		return messageConverter;
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		Jackson2ObjectMapperBuilder builder = 
+				new Jackson2ObjectMapperBuilder()
+				.indentOutput(true)
+				.dateFormat(new SimpleDateFormat("yyyy-mm-dd"));
+
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+						new MediaType("application", "json", Charset.forName("utf-8"))
+						)
+				);
+
+		return messageConverter;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(stringHttpMessageConverter());
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
+
 }
